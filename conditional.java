7829c1156ec4236da78faf8d52f8acc0be6c8f9e
Rahulkumar <rk3235022@gmail.com>
2023-07-25 12:31:37 +0530

    i am learning

--- conditional.java
+++ conditional.java
@@ -17,5 +17,24 @@ public class conditional {
         }else{
             System.out.println("you are in category of senior citizen");
         }
+        
+
+        // conditional statement no. two
+        System.out.println("Enter number one");
+        int num1=sc.nextInt();
+        System.out.println("Enter number two");
+        int num2=sc.nextInt();
+        System.out.println("Enter number three");
+        int num3=sc.nextInt();
+    
+        if(num1>num2 && num1>num3 ){
+            System.out.println("number one is greater");
+        }else if(num2>num1 && num2>num3){
+            System.out.println("number two is greater");
+        }else{
+            System.out.println("number three is greater");
+        }
     }
+
+
 }
